4a8af78d2888786ba7a8314f048af5c6e84ae31d
Marcello1410 <mher0031@student.monash.edu>
2025-05-20 13:21:53 +1000

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,79 @@
+/*navigations*/
+// add classes for mobile navigation toggling
+    var CSbody = document.querySelector("body");
+    const CSnavbarMenu = document.querySelector("#cs-navigation");
+    const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
+
+    CShamburgerMenu.addEventListener('click', function() {
+        CShamburgerMenu.classList.toggle("cs-active");
+        CSnavbarMenu.classList.toggle("cs-active");
+        CSbody.classList.toggle("cs-open");
+        // run the function to check the aria-expanded value
+        ariaExpanded();
+    });
+
+    // checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
+    function ariaExpanded() {
+        const csUL = document.querySelector('#cs-expanded');
+        const csExpanded = csUL.getAttribute('aria-expanded');
+
+        if (csExpanded === 'false') {
+            csUL.setAttribute('aria-expanded', 'true');
+        } else {
+            csUL.setAttribute('aria-expanded', 'false');
+        }
+    }
+
+        // This script adds a class to the body after scrolling 100px
+    // and we used these body.scroll styles to create some on scroll 
+    // animations with the navbar
+    
+    document.addEventListener('scroll', (e) => { 
+        const scroll = document.documentElement.scrollTop;
+        if(scroll >= 100){
+    document.querySelector('body').classList.add('scroll')
+        } else {
+        document.querySelector('body').classList.remove('scroll')
+        }
+    });
+
+
+    // mobile nav toggle code
+    const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+        for (const item of dropDowns) {
+            const onClick = () => {
+            item.classList.toggle('cs-active')
+        }
+        item.addEventListener('click', onClick)
+        }                           
+
+/*slideshow*/
+let slideIndex = [1,1];
+let slideId = ["mySlides1"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block"; 
+}
+
+Fancybox.bind( "[data-fancybox=gallery]" ); 
+$( 'button' ).click( filterItems ); 
+function filterItems() {
+    let button = this;
+    let selector = $( button ).data( 'filter' );
+    console.log( selector );
+    $( '#products img' ).not( selector ).hide(500);
+    $( selector ).show(500);
+    }
